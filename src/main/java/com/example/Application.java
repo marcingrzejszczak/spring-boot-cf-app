e9b30a7672142649f5af6cea39cce764add02e95
Marcin Grzejszczak <marcin@grzejszczak.pl>
2017-01-23 13:22:58 +0100

    Added an endpoint ot kill the application

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@GetMapping
 	String hello() {
+		log.info("Received a request!");
 		return "Hello from instance id [" + System.getenv("CF_INSTANCE_INDEX") + "]";
+	}
+
+	@GetMapping("/kill")
+	void kill() {
+		log.info("Will kill the application :(");
+		System.exit(1);
 	}
 }
